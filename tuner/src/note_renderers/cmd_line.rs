eedff11e77946e1a8ec7ee7d25da2a1d00643520
Marco Herrera-Rendon <mherrerarendon@makemusic.com>
2024-10-01 15:14:27 -0600

    building the tuner string

--- tuner/src/note_renderers/cmd_line.rs
+++ tuner/src/note_renderers/cmd_line.rs
@@ -17,16 +17,12 @@ struct TunerLayout {
     cursor_pos: usize,
     previous_note_pos: usize,
     next_note_pos: usize,
+    width: usize,
 }
 
 impl TunerLayout {
     fn new(note: &NoteDetectionResult, terminal_width: usize) -> Self {
-        let actual_terminal_width = if terminal_width % 2 == 1 {
-            terminal_width
-        } else {
-            terminal_width - 1
-        };
-        let note_name_pos = actual_terminal_width / 2;
+        let note_name_pos = terminal_width / 2;
         let ticks_mark_percent =
             (NUM_CENTS_BETWEEN_NOTES - MAX_CENTS_OFFSET) / NUM_CENTS_BETWEEN_NOTES;
         let left_tick_pos = (note_name_pos as f64 * ticks_mark_percent).round() as usize;
@@ -40,8 +36,26 @@ impl TunerLayout {
             note_name_pos,
             cursor_pos,
             previous_note_pos: 0,
-            next_note_pos: actual_terminal_width - 1,
+            next_note_pos: terminal_width - 1,
+            width: terminal_width,
         }
+    }
+
+    fn build(&self, note: &NoteDetectionResult) -> String {
+        let mut s = String::with_capacity(self.width);
+        macro_rules! repeating_str {
+            ($the_str:expr, $count:expr) => {{
+                &std::iter::repeat($the_str).take($count).collect::<String>()
+            }};
+        }
+        s.push_str(&note.previous_note_name.to_string());
+        s.push_str(repeating_str!(" ",));
+        s.push_str(std::iter::repeat(".").take(self.note_name_pos - self.left_tick_pos));
+        s.push_str(&note.note_name.to_string());
+        s.push_str(std::iter::repeat(".").take(self.right_tick_pos - self.note_name_pos));
+        s.push_str(std::iter::repeat(" ").take(self.next_note_pos - self.right_tick_pos));
+        s.push_str(&note.next_note_name.to_string());
+        s
     }
 }
 
@@ -49,27 +63,36 @@ pub struct CmdLineNoteRenderer {
     width: usize,
 }
 
-impl CmdLineNoteRenderer {
-    fn clear_terminal() {
-        print!("{}[2J", 27 as char);
-    }
-}
-
 impl NoteRenderer for CmdLineNoteRenderer {
     fn render_note(&mut self, note: NoteDetectionResult) -> anyhow::Result<()> {
-        let mut tuner_str_format = String::new();
-
-        // subtracting 2 to account for the previous and next note names
-        // fill size will be used twice, once per side of the current note name
-        let fill = self.width - 2;
-        write!(
-            tuner_str_format,
-            "{}{:^width$}",
-            note.previous_note_name,
-            width = fill
-        )?;
+        let mut tuner_str_format = std::iter::repeat(" ").take(self.width).collect::<String>();
         let tuner_layout = TunerLayout::new(&note, self.width);
+        tuner_str_format.replace_range(
+            tuner_layout.previous_note_pos..tuner_layout.previous_note_pos,
+            &note.previous_note_name.to_string(),
+        );
+        tuner_str_format.replace_range(
+            tuner_layout.left_tick_pos..tuner_layout.right_tick_pos,
+            &note.previous_note_name.to_string(),
+        );
+        tuner_str_format.replace_range(
+            tuner_layout.previous_note_pos..tuner_layout.previous_note_pos,
+            &note.previous_note_name.to_string(),
+        );
+        tuner_str_format.replace_range(
+            tuner_layout.previous_note_pos..tuner_layout.previous_note_pos,
+            &note.previous_note_name.to_string(),
+        );
+        tuner_str_format.replace_range(
+            tuner_layout.previous_note_pos..tuner_layout.previous_note_pos,
+            &note.previous_note_name.to_string(),
+        );
+        tuner_str_format.replace_range(
+            tuner_layout.previous_note_pos..tuner_layout.previous_note_pos,
+            &note.previous_note_name.to_string(),
+        );
         let mut stdout = std::io::stdout().lock();
+        write!(stdout, "{}[2J", 27 as char)?;
         write!(stdout, "")?;
         Ok(())
     }
